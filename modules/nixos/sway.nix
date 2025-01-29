71618f896268b8538c8042ed44fb711a5a0c60ac
Koye64 <mail@koye64.com>
2025-01-29 16:39:05 -0700

    add more wayland config

--- modules/nixos/sway.nix
+++ modules/nixos/sway.nix
@@ -1,8 +1,21 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
+  environment.systemPackages = with pkgs; [
+    libsForQt5.qt5.qtwayland
+    kdePackages.qtwayland
+  ];
+
   programs.sway = {
     enable = true;
+    extraPackages = [ ];
+    extraSessionCommands = ''
+      export SDL_VIDEODRIVER=wayland
+      # QT (needs qt5.qtwayland in systemPackages):
+      export QT_QPA_PLATFORM=wayland-egl
+      export QT_WAYLAND_DISABLE_WINDOWDECORATION="1"
+      export _JAVA_AWT_WM_NONREPARENTING=1
+    '';
   };
 
   programs.waybar = {

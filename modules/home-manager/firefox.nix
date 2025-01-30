704acdf8102571332e2eb34eda612c6d372e8026
Koye64 <mail@koye64.com>
2025-01-30 04:06:18 -0700

    configure Firefox

--- modules/home-manager/firefox.nix
+++ modules/home-manager/firefox.nix
@@ -1,7 +1,25 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
   programs.firefox = {
     enable = true;
+    profiles = {
+      default = {
+        id = 0;
+        extensions = with pkgs.nur.repos.rycee.firefox-addons; [
+	  catppuccin-gh-file-explorer
+	  ublock-origin
+	  darkreader
+	  firefox-color
+	  stylus
+	  enhancer-for-youtube
+	  return-youtube-dislikes
+	];
+	settings = {
+	  "middlemouse.paste" = false;
+	  "general.autoScroll" = true;
+	};
+      };
+    };
   };
 }

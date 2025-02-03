29e3c0cafec102f6901f68ce146f8020b800210a
Koye64 <mail@koye64.com>
2025-02-02 23:30:46 -0700

    add Powerlevel10k to ZSH

--- modules/home-manager/console/zsh.nix
+++ modules/home-manager/console/zsh.nix
@@ -1,11 +1,33 @@
 { pkgs, ... }:
 
 {
+  home.file.".p10k.zsh" = {
+    enable = true;
+    executable = true;
+    source = ./p10k.zsh;
+    target = ".p10k.zsh";
+  };
+
   programs.zsh = {
     enable = true;
     enableCompletion = true;
     enableVteIntegration = true;
     autocd = true;
+
+    initExtraFirst = ''
+      if [[ -r "''${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-''${(%):-%n}.zsh" ]]; then
+        source "''${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-''${(%):-%n}.zsh"
+      fi
+    '';
+    initExtra = ''[[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh'';
+
+    plugins = [
+      {
+        name = "powerlevel10k";
+	file = "powerlevel10k.zsh-theme";
+	src = "${pkgs.zsh-powerlevel10k}/share/zsh-powerlevel10k/";
+      }
+    ];
     
     history = {
       append = true;

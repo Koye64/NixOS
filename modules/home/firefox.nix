3cdc5659851925c334684b05e4317ea50d5b6218
df-salaz <df.salaz@gmail.com>
2025-02-27 23:54:10 -0700

    Update Firefox config

--- modules/home/firefox.nix
+++ modules/home/firefox.nix
@@ -1,25 +1,23 @@
-{ pkgs, ... }:
-
-{
+{pkgs, ...}: {
   programs.firefox = {
     enable = true;
     profiles = {
       default = {
         id = 0;
-        extensions = with pkgs.nur.repos.rycee.firefox-addons; [
-	  catppuccin-gh-file-explorer
-	  ublock-origin
-	  darkreader
-	  firefox-color
-	  stylus
-	  enhancer-for-youtube
-	  return-youtube-dislikes
-	  sponsorblock
-	];
-	settings = {
-	  "middlemouse.paste" = false;
-	  "general.autoScroll" = true;
-	};
+        extensions.packages = with pkgs.nur.repos.rycee.firefox-addons; [
+          catppuccin-gh-file-explorer
+          ublock-origin
+          darkreader
+          firefox-color
+          stylus
+          enhancer-for-youtube
+          return-youtube-dislikes
+          sponsorblock
+        ];
+        settings = {
+          "middlemouse.paste" = false;
+          "general.autoScroll" = true;
+        };
       };
     };
   };

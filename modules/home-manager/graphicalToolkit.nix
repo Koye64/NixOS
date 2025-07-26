8d16d7b0ad66683970454ba7ad63d494ad7c8ab6
Koye64 <mail@koye64.com>
2025-07-25 18:41:45 -0700

    Prefer dark GTK theme

--- modules/home-manager/graphicalToolkit.nix
+++ modules/home-manager/graphicalToolkit.nix
@@ -8,6 +8,19 @@
     name = "Bibata-Modern-Classic";
     size = 24;
   };
-  gtk = { enable = true; };
+  dconf.settings = {
+    "org/gnome/desktop/interface" = {
+      color-scheme = "prefer-dark";
+    };
+  };
+  gtk = let
+    extraConfig = {
+      gtk-application-prefer-dark-theme = 1;
+    };
+  in {
+    enable = true;
+    gtk3 = { inherit extraConfig; };
+    gtk4 = { inherit extraConfig; };
+  };
   qt = { enable = true; };
 }

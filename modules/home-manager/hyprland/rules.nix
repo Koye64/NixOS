f14b55c93401e0b5080e4f84af1b33f5ce19e284
Koye64 <mail@koye64.com>
2025-08-02 00:23:34 -0700

    Hyprland disable rounding in Steam

--- modules/home-manager/hyprland/rules.nix
+++ modules/home-manager/hyprland/rules.nix
@@ -5,6 +5,7 @@
       "suppressevent maximize, class:.*"
       "nofocus,class:^$,title:^$,xwayland:1,floating:1,fullscreen:0,pinned:0"
       "immediate, class:.*"
+      "rounding 0, class:steam"
     ];
     workspace = [
       "1, monitor:HDMI-A-2"

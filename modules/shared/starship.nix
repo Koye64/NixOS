e3109110dc1ee9b1a236f253213fd39e91152300
Koye64 <mail@koye64.com>
2025-08-15 15:59:24 -0700

    Adjust starship icon spacing

--- modules/shared/starship.nix
+++ modules/shared/starship.nix
@@ -62,11 +62,11 @@
     truncation_length = 3;
     truncation_symbol = "…/";
     substitutions = {
-      "Documents" = "󰈙 ";
-      "Downloads" = " ";
-      "Music" = "󰝚 ";
-      "Pictures" = " ";
-      "Developer" = "󰲋 ";
+      "Documents" = "󰈙";
+      "Downloads" = "";
+      "Music" = "󰝚";
+      "Pictures" = "";
+      "Developer" = "󰲋";
     };
   };
   git_branch = {

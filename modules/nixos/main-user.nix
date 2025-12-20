113343c61720dc2aaaf5d6a0300c860a5bdc48f7
Koye <me@koye64.com>
2025-12-20 14:46:57 -0700

    Add proper user description

--- modules/nixos/main-user.nix
+++ modules/nixos/main-user.nix
@@ -18,6 +18,13 @@ in {
       '';
     };
 
+    userDescription = lib.mkOption {
+      default = "User";
+      description = ''
+        user's description (full name)
+      '';
+    };
+
     shell = lib.mkOption {
       default = pkgs.fish;
       description = ''
@@ -36,7 +43,7 @@ in {
   config = lib.mkIf cfg.enable {
     users.users.${cfg.userName} = {
       isNormalUser = true;
-      description = "Main user";
+      description = cfg.userDescription;
       inherit (cfg) shell;
       extraGroups = ["wheel"];
     };
